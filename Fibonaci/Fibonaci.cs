ce3d594254009bddcb4c74f302c41f0114f75fa1
Osvaldo Luna <osvaldounam18@gmail.com>
2017-11-16 20:08:21 -0600

    Serie de Fibonaci que para hasta el numero de elementos de la serie

---
+++ Fibonaci/Fibonaci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pnum = 1, snum = 1, res;
+            Console.WriteLine("Ingrese los digitos deseados para la serie:");
+            int fib = int.Parse(Console.ReadLine());
+            Console.WriteLine("---La serie Fibonacci es--- ");
+            Console.Write(pnum + " , " + snum);
+            while (fib != 0)
+            {
+                res = snum;
+                Console.Write(" , " + (pnum + snum));
+                snum = res + pnum;
+                pnum = res;
+                fib--;
+            }
+            Console.Write(".");
+            Console.ReadKey();
+        }
+    }
+}
